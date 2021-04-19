48487ef438af3eba05e99ebaf35518885ab3b11c
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2021-04-19 19:53:20 +0200

    test(localhost-screenshot): added test setup

--- packages/localhost-screenshot/__tests__/localhost-screenshot.test.js
+++ packages/localhost-screenshot/__tests__/localhost-screenshot.test.js
@@ -1,7 +1,32 @@
-'use strict';
+const { join } = require('path');
+const { fs, vol } = require('memfs');
 
-const localhostScreenshot = require('..');
+const TMP = '/tmp';
+const WORKDIR = '/src';
+const DIST_DIR = './dist';
 
-describe('localhost-screenshot', () => {
-    it('needs tests');
+const INDEX_HTML =
+  '<html><head><title>Demo</title></head><body>Hello, world!</body></html>';
+
+jest.mock('fs', () => require('memfs').vol);
+
+describe('Localhost Screenshot', () => {
+  beforeAll(() => {
+    process.env.RUNNER_TEMP = TMP;
+    process.env.RUNNER_WORKSPACE = WORKDIR;
+
+    const files = {
+      './index.html': INDEX_HTML,
+    };
+
+    vol.fromJSON(files, join(WORKDIR, DIST_DIR));
+  });
+
+  it('creates screenshot using default options', async () => {
+    await import('../src');
+
+    expect(fs.readFileSync(join(WORKDIR, './dist/index.html'), 'utf8')).toEqual(
+      INDEX_HTML,
+    );
+  });
 });
