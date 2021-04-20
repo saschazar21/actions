2e57546b24595e527b98777192f3c1ab4040e90e
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2021-04-20 12:54:27 +0200

    test(localhost-screenshot): added tests

--- packages/localhost-screenshot/__tests__/localhost-screenshot.test.js
+++ packages/localhost-screenshot/__tests__/localhost-screenshot.test.js
@@ -1,32 +1,61 @@
-const { join } = require('path');
-const { fs, vol } = require('memfs');
+/* eslint-disable import/no-extraneous-dependencies */
+const fs = require('fs');
+const fetch = require('node-fetch');
 
-const TMP = '/tmp';
-const WORKDIR = '/src';
+// jest.mock('fs', () => require('memfs').fs);
+
+const TMP = '/memfs';
 const DIST_DIR = './dist';
 
-const INDEX_HTML =
-  '<html><head><title>Demo</title></head><body>Hello, world!</body></html>';
+const DEFAULT_OPTIONS = {
+  dark: false,
+  devices: undefined,
+  dist: DIST_DIR,
+  name: 'screenshot',
+  url: '/',
+};
 
-jest.mock('fs', () => require('memfs').vol);
+process.env.HOME = TMP;
+process.env.GITHUB_WORKSPACE = __dirname;
+
+jest.mock('fs', () => {
+  const Volume = jest.requireActual('memfs').Volume;
+  const fs = jest.requireActual('fs');
+  const ufs = jest.requireActual('unionfs').ufs;
+
+  const vol = Volume.fromJSON({ './.DS_STORE': '' }, TMP);
+  return ufs.use(fs).use(vol);
+});
+
+const screenshot = require('../src/screenshot');
+const server = require('../src/server');
 
 describe('Localhost Screenshot', () => {
-  beforeAll(() => {
-    process.env.HOME = TMP;
-    process.env.GITHUB_WORKSPACE = WORKDIR;
+  let host;
+  let proc;
 
-    const files = {
-      './index.html': INDEX_HTML,
-    };
+  afterAll(() => proc.kill());
 
-    vol.fromJSON(files, join(WORKDIR, DIST_DIR));
+  beforeAll(async () => {
+    const s = await server({ dist: './' });
+    host = s[1];
+    proc = s[0];
   });
 
-  it('creates screenshot using default options', async () => {
-    await import('../src');
+  it('starts server process and kills it', async () => {
+    expect(proc).toBeTruthy();
+    expect(/^http:\/\/localhost:\d{2,5}$/.test(host)).toBeTruthy();
 
-    expect(fs.readFileSync(join(WORKDIR, './dist/index.html'), 'utf8')).toEqual(
-      INDEX_HTML,
-    );
+    const response = await fetch(host);
+
+    expect(response.status).toEqual(200);
+  });
+
+  it('takes screenshot using default options', async () => {
+    await screenshot({ ...DEFAULT_OPTIONS, baseUrl: host });
+
+    expect(
+      fs.readFileSync('/memfs/screenshot_1440x900.png', null),
+    ).toBeTruthy();
   });
 });
